c5fd4ecb47dfb31e32d36098fcf9461d0410e750
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-23 16:23:52 +0530

    Added fraamework

---
+++ TabBar/TabBar/TabBar.h
@@ -0,0 +1,19 @@
+//
+//  TabBar.h
+//  TabBar
+//
+//  Created by Manish on 23/08/20.
+//  Copyright © 2020 Manish. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TabBar.
+FOUNDATION_EXPORT double TabBarVersionNumber;
+
+//! Project version string for TabBar.
+FOUNDATION_EXPORT const unsigned char TabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TabBar/PublicHeader.h>
+
+
