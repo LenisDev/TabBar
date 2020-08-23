b61e959db4ee2de1b1e80f28c24831eeb4ccb18a
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-23 18:50:45 +0530

    Added tab bar item view and view model

---
+++ TabBar/TabBar/Implementations/TabBarItemView.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarItemView.swift
+//  TabBar
+//
+//  Created by Manish on 23/08/20.
+//  Copyright © 2020 Manish. All rights reserved.
+//
+
+import UIKit
+
+class TabBarItemView: BaseView<TabBarItemViewModel> {
+    
+    
+    
+}
