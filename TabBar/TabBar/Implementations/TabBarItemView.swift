550c49bc123d69f5cb90928986a4b739cf90005c
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-23 20:34:57 +0530

    Added setups for tab bar item view

--- TabBar/TabBar/Implementations/TabBarItemView.swift
+++ TabBar/TabBar/Implementations/TabBarItemView.swift
@@ -13,14 +13,66 @@ public class TabBarItemView: BaseView<TabBarItemViewModel> {
     private(set) lazy var titleLbl = UILabel()
     private(set) lazy var imageView = UIImageView()
     
+    private(set) lazy var labelImageSV = UIStackView(arrangedSubViews: [],
+                                                     spacing: 10,
+                                                     axis: .horizontal,
+                                                     distribution: .fillProportionally)
+    
     override func setupViews() {
         super.setupViews()
+        
+        if data.title == nil {
+            self.setupImageViewLblOnly()
+        } else if data.image == nil {
+            self.setupTitleLblOnly()
+        } else {
+            // both can not be nil -> view model will not allow
+            self.setupTitleLblAndImageView()
+        }
         
     }
     
     override func setupData() {
         super.setupData()
         
+        self.titleLbl.text = data.title
+        self.imageView.image = data.image
+    }
+    
+}
+
+// MARK: - Setups
+extension TabBarItemView {
+    
+    private func setupTitleLblOnly() {
+        titleLbl.textAlignment = .center
+        titleLbl.sameSize(as: rootView)
+    }
+    
+    private func setupImageViewLblOnly() {
+        imageView.contentMode = .scaleToFill
+        imageView.center(to: rootView)
+    }
+    
+    private func setupTitleLblAndImageView() {
+        labelImageSV.removeAllArrangedSubviews()
+        
+        labelImageSV.addArrangedSubview(titleLbl)
+        labelImageSV.addArrangedSubview(imageView)
+        
+        labelImageSV.center(to: rootView)
+    }
+    
+}
+
+// MARK: - Extend
+extension TabBarItemView {
+    
+    public func tintColor(_ color: UIColor) -> TabBarItemView {
+        self.titleLbl.textColor = color
+        self.imageView.tintColor = color
+        
+        return self
     }
     
 }
