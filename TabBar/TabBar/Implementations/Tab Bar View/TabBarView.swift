519609982c94582bfa8513640eea874ed0ad8b51
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-23 23:24:38 +0530

    Added item style functionality in root tab bar

--- TabBar/TabBar/Implementations/Tab Bar View/TabBarView.swift
+++ TabBar/TabBar/Implementations/Tab Bar View/TabBarView.swift
@@ -13,18 +13,62 @@ public class TabBarView: BaseView<TabBarViewModel> {
     private(set) lazy var rootSV = UIStackView(arrangedSubViews: [],
                                                axis: .horizontal,
                                                distribution: .fillEqually)
+    
+    // MARK: - Properties
+    private(set) lazy var itemViews = [TabBarItemView]()
+    private(set) var onItemSelected: SelectedTabItem
+    private(set) var itemStyle: Stylable = CapsuleShapeStyle()
+    
+    // MARK: - Init
+    public init(data: TabBarViewModel,
+                onItemSelected: @escaping SelectedTabItem) {
+        self.onItemSelected = onItemSelected
         
+        super.init(data: data)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setups
     override func setupViews() {
         super.setupViews()
     
         self.rootSV.removeAllArrangedSubviews()
-        data.items.forEach { item in
-            self.rootSV.addArrangedSubview(TabBarItemView(data: item) { [weak self] selectedItem in
-                self?.data.select(item: selectedItem)
-            })
+        self.rootSV.sameSize(as: rootView).with(height: 40)
+    }
+    
+    override func setupData() {
+        super.setupData()
+        
+        itemViews = data.items.map { TabBarItemView(data: $0) { [weak self] selectedItem in
+            self?.data.select(item: selectedItem)
+            
+            self?.onItemSelected(selectedItem)
+        }}
+        
+        // setup item style
+        itemStyle(itemStyle)
+        
+        self.rootSV.add(arrangedSubViews: itemViews)
+    }
+    
+}
+
+// MARK: - Style
+public extension TabBarView {
+    
+    @discardableResult
+    func itemStyle(_ style: Stylable) -> Self {
+        
+        self.itemStyle = style
+        
+        itemViews.forEach { iv in
+            iv.style(style)
         }
         
-        self.rootSV.sameSize(as: rootView).with(height: 40)
+        return self
     }
     
 }
