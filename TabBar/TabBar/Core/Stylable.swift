73e8b5d6d5ab1d3b7dee5ef7048ee6e797c130ef
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-24 10:26:24 +0530

    Added docs to Stylable

--- TabBar/TabBar/Core/Stylable.swift
+++ TabBar/TabBar/Core/Stylable.swift
@@ -8,55 +8,23 @@
 
 import UIKit
 
+/// Custom style used for styling views.
+///
+/// - Important
+///  - It will override any existing property matching with `Stylable` property
+///
 public protocol Stylable {
     
+    /// Background color
     var bgColor: UIColor { get }
     
+    /// Tint color, applied on child views.
     var tintColor: UIColor { get }
     
+    /// Border around view
     var border: Border { get }
     
+    /// Corner radius on view
     var radius: Radius { get }
-    
-}
-
-// MARK: - Border
-public struct Border {
-    let color: UIColor
-    let width: CGFloat
-    let edges: [UIRectEdge]
-    
-    public init (color: UIColor = .black,
-                 width: CGFloat = 1,
-                 edges: [UIRectEdge] = [.all]) {
-        
-        self.color = color
-        self.width = width
-        self.edges = edges
-    }
-}
-
-public extension Border {
-    
-    static var none: Border { Border(color: .clear, width: 0, edges: [.all]) }
-    
-}
-
-// MARK: - Radius
-public struct Radius {
-    let size: CGFloat
-    let corners: [UIRectCorner]
-    
-    public init (size: CGFloat = 15,
-                 corners: [UIRectCorner] = [.allCorners]) {
-        
-        self.size = size
-        self.corners = corners
-    }
-}
-
-extension Radius {
-    
-    static var none: Radius { Radius(size: 0, corners: [.allCorners]) }
     
 }
