c8200ffbc7d6dcbe6c2a4f6e03e99144f10462b3
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-24 10:46:30 +0530

    Added docs to base view

--- TabBar/TabBar/Core/BaseView.swift
+++ TabBar/TabBar/Core/BaseView.swift
@@ -8,15 +8,33 @@
 
 import UIKit
 
+/// Provides basic functionalities for data driven views. It confirms to DesignableView, that provides design related functionalities.
+///
+/// # Main Features
+///  - generic data confirming to `BaseViewModel`
+///  - `setupViews`
+///  - `setupData`
+///
+/// # Important
+///  - Any view that  needs to designable and data driven, must use `BaseView` as it's parent class
+///  - `rootView` should be considered as top view and all subview at top level (hierarchy) should be added to `rootView`
+///
 public class BaseView<DATA: BaseViewModel>: UIView, DesignableView {
     
     // MARK: - Properties
+    
+    /// Generic data that confirms to `BaseViewModel`
+    ///
+    /// # Important
+    /// - Everytime data is set, view will reset
+    ///
     var data: DATA {
         didSet {
             self.initialize()
         }
     }
     
+    /// Top view
     private(set) var rootView = UIView()
     
     // MARK: - Inits
@@ -38,11 +56,21 @@ public class BaseView<DATA: BaseViewModel>: UIView, DesignableView {
         self.setupData()
     }
     
+    /// Used to setup views
+    ///
+    /// # Important
+    /// - Called when data is set
+    ///
     func setupViews() {
         self.rootView.subviews.forEach { $0.removeFromSuperview() }
         self.rootView.sameSize(as: self)
     }
     
+    /// Used to setup data on views
+    ///
+    /// # Important
+    /// - Called when data is set
+    ///
     func setupData() {
         
     }
