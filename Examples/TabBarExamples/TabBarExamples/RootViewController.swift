322843f9b927e1e57c661fd287181827a83928ef
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-24 23:07:39 +0530

    Added styles and constants to example project

--- Examples/TabBarExamples/TabBarExamples/RootViewController.swift
+++ Examples/TabBarExamples/TabBarExamples/RootViewController.swift
@@ -11,29 +11,23 @@ import TabBar
 
 class RootViewController: UIViewController {
 
-    let tabView = TabBarView(data: TabBarViewModel(items: [TabBarItemViewModel(id: UUID().description,
-                                                                               title: "Tab 1",
-                                                                               image: UIImage(systemName: "trash"),
-                                                                               state: .selected),
-                                                           TabBarItemViewModel(id: UUID().description,
-                                                                               title: "Tab 2",
-                                                                               image: nil)])) { selectedItem in
-                                                                                print(selectedItem)
+    let tabView = TabBarView(data: TabBarViewModel(items: tabBarItemTitleImage)) { selectedItem in
+        print(selectedItem)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.view.backgroundColor = .yellow
 
         tabView
             .center(to: self.view)
             .with(width: 300)
 
         tabView
-            .style(CapsuleShapeStyle())
-//            .itemSelectedStyle(CapsuleShapeSelectedStyle())
-        //            .bgColor(.red)
-        //            .tintColor(.white)
-        //            .radius(Radius(size: 20, corners: [.allCorners]))
+            .style(RectStyle())
+            .itemUnselectedStyle(RectStyle())
+            .itemSelectedStyle(RectBottomBorderStyle())
 
     }
 
---
+++ Examples/TabBarExamples/TabBarExamples/Styles/RectStyle.swift
@@ -0,0 +1,22 @@
+//
+//  RectStyle.swift
+//  TabBarExamples
+//
+//  Created by Manish on 24/08/20.
+//  Copyright © 2020 Manish. All rights reserved.
+//
+
+import UIKit
+import TabBar
+
+struct RectStyle: Stylable {
+
+    var bgColor: UIColor { .white }
+
+    var tintColor: UIColor { .black }
+
+    var border: Border { .none }
+
+    var radius: Radius { .none }
+
+}
